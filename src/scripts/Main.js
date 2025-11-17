489fe67108221b088b95308f49fd5d1fbe23323d
bloodflowbozo <zacklamontagne0@gmail.com>
2025-11-17 11:59:05 -0500

    fin du code

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,4 +1,5 @@
 import Icons from './utils/Icons.js';
+import Swiper from 'swiper/bundle';
 
 class Main {
   constructor() {
@@ -7,6 +8,15 @@ class Main {
 
   init() {
     Icons.load();
+    this.SwiperManager();
+  }
+
+  SwiperManager() {
+    const swiper = new Swiper('.swiper', {
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
   }
 }
 new Main();
